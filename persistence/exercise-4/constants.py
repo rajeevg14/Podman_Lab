769b614c91faa54634834486514fddb9bcc646bf
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-08-21 16:20:17 -0400

    Persistence Lab

---
+++ persistence/exercise-4/constants.py
@@ -0,0 +1,13 @@
+__author__ = 'Rajeev Kumar Gupta'
+
+from os import environ
+ERR_NO_SOURCE = 'No sources defined! Set a source using /source list, of, sub, reddits'
+skip_list = []
+sources_dict = {}
+
+BOT_KEY = environ.get('NBT_ACCESS_TOKEN')
+REDDIT_CLIENT_ID = environ.get('REDDIT_CLIENT_ID')
+REDDIT_CLIENT_SECRET = environ.get('REDDIT_CLIENT_SECRET')
+API_BASE = 'https://api.telegram.org/bot'
+UPDATE_PERIOD = 6
+FALSE_RESPONSE = {"ok": False}
