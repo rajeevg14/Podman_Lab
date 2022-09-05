5bc0e9e360db4d2399f4b89ba27acaba08dae9a2
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-09-05 10:15:32 -0400

    c

---
+++ hpc/c/c1/run.c
@@ -0,0 +1,10 @@
+// C program to implement
+// the above approach
+#include <stdio.h>
+  
+// Driver code
+int main()
+{
+    printf("Welcome to Podman World!!!\n");
+    return 0;
+}
---
+++ hpc/cpp/main.cpp
@@ -0,0 +1,15 @@
+// C program to implement
+// the above approach
+#include <stdio.h>
+  
+// Driver code
+int main()
+{
+    printf("Welcome to Podman World!!!\n");a#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+    return 0;
+}
