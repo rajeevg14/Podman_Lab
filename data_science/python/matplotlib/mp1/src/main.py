19d43d7321d071ec160a1cf8a75f48037116fe67
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-10-27 22:00:08 -0400

    m;p

--- data_science/python/matplotlib/mp1/src/main.py
+++ data_science/python/matplotlib/mp1/src/main.py
@@ -1,3 +1,16 @@
 import numpy as np
+from scipy.interpolate import splprep, splev
 
-print(np.sqrt(2))
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
+
+N = 400
+t = np.linspace(0, 3 * np.pi, N)
+r = 0.5 + np.cos(t)
+x, y = r * np.cos(t), r * np.sin(t)
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.xlabel("X value")
+plt.ylabel("Y value")
+plt.savefig('1.png')
