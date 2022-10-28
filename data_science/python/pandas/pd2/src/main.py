57886196cc76eecca292a9f3ac0d6365a4d73ec8
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-10-27 21:38:35 -0400

    np

--- data_science/python/pandas/pd2/src/main.py
+++ data_science/python/pandas/pd2/src/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv(
     "../input/datasets/book_sales.csv",
     index_col='Date',
     parse_dates=['Date'],
 ).drop('Paperback', axis=1)
+
+df['Time'] = np.arange(len(df.index))
 
 print(df.head())
