62bd038bc9250ef009747a9f974f0231a29e4703
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-10-29 20:57:01 -0400

    ff

--- data_science/python/matplotlib/mp1/src/main.py
+++ data_science/python/matplotlib/mp1/src/main.py
@@ -1,16 +1,65 @@
-import numpy as np
-from scipy.interpolate import splprep, splev
+#Flask imports
+from flask import Flask, render_template, send_file, make_response, url_for, Response
 
+#Pandas and Matplotlib
+import pandas as pd
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.path import Path
-from matplotlib.patches import PathPatch
 
-N = 400
-t = np.linspace(0, 3 * np.pi, N)
-r = 0.5 + np.cos(t)
-x, y = r * np.cos(t), r * np.sin(t)
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.xlabel("X value")
-plt.ylabel("Y value")
-plt.savefig('1.png')
+#other requirements
+import io
+
+#Data imports
+
+#from GetFixtres import ECS_data
+ECS_data = pd.read_csv("/home/jasher4994/mysite/ECS_data.csv")
+#from GetFixtures2 import GK_roi
+GK_roi = pd.read_csv("/home/jasher4994/mysite/GK_roi.csv")
+
+
+app = Flask(__name__)
+
+#Pandas Page
+@app.route('/')
+@app.route('/pandas', methods=("POST", "GET"))
+def GK():
+    return render_template('pandas.html',
+                           PageTitle = "Pandas",
+                           table=[GK_roi.to_html(classes='data', index = False)], titles= GK_roi.columns.values)
+
+
+#Matplotlib page
+@app.route('/matplot', methods=("POST", "GET"))
+def mpl():
+    return render_template('matplot.html',
+                           PageTitle = "Matplotlib")
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig, ax = plt.subplots(figsize = (6,4))
+    fig.patch.set_facecolor('#E8E5DA')
+
+    x = ECS_data.team
+    y = ECS_data.gw1
+
+    ax.bar(x, y, color = "#304C89")
+
+    plt.xticks(rotation = 30, size = 5)
+    plt.ylabel("Expected Clean Sheets", size = 5)
+
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
