86283cc42f5be3c2793e41a178fb86e13402d0a2
rajeevg14 <92331412+rajeevg14@users.noreply.github.com>
2022-10-29 20:41:18 -0400

    mps

---
+++ data_science/python/matplotlib/mp4/src/main.py
@@ -0,0 +1,19 @@
+import io
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from flask import Flask
+import numpy as np
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+app = Flask(__name__)
+@app.route('/')
+def plot_png():
+   fig = Figure()
+   axis = fig.add_subplot(1, 1, 1)
+   xs = np.random.rand(100)
+   ys = np.random.rand(100)
+   axis.plot(xs, ys)
+   output = io.BytesIO()
+   FigureCanvas(fig).print_png(output)
+   return Response(output.getvalue(), mimetype='image/png')
